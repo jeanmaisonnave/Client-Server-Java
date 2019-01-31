d9c3dd5a4f5e73f2c3f5c2fdb1266c18af5b3432
Joe <44689731+joedioufy@users.noreply.github.com>
2019-01-31 14:23:07 +0100

    Joe
    Les classes recettes et etiquettes

---
+++ projetJava/bin/projetJava/Recette.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Joe
+ */
+public class Recette {
+    
+     private int idRecette;
+    private String nomRecette;
+    private String libelle;
+    private int ingredient;
+    private static int nbreRecette = 0;
+    
+    public Recette() {
+        //contructeur
+        //On incrémente la variable à chaque appel du constructeur
+        nbreRecette++;
+    }
+    public Recette(int idRecette, String nomRecette, String libelle, int ingredient) {
+        //contructeur
+        //On incrémente la variable à chaque appel du constructeur
+        nbreRecette++;
+    }
+
+    public int getIdRecette() {
+        return idRecette;
+    }
+
+    public void setIdRecette(int idRecette) {
+        this.idRecette = idRecette;
+    }
+
+    public String getNomRecette() {
+        return nomRecette;
+    }
+
+    public void setNomRecette(String nomRecette) {
+        this.nomRecette = nomRecette;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(int ingredient) {
+        this.ingredient = ingredient;
+    }
+            
+}
