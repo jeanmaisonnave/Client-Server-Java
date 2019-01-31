50c31f4e00d5a88c57fe009ab87dd5d7d679cc75
EnautAH <47215161+EnautAH@users.noreply.github.com>
2019-01-31 14:22:01 +0100

    Add files via upload

---
+++ projetJava/bin/projetJava/Categorie.java
@@ -0,0 +1,23 @@
+
+public class Categorie {
+
+	//attributs
+		private int idCategorie;
+		private String libCategorie;
+		
+	//getters et setters
+		public int getIdCategorie() {
+			return idCategorie;
+		}
+		public void setIdCategorie(int idCategorie) {
+			this.idCategorie = idCategorie;
+		}
+		public String getLibCategorie() {
+			return libCategorie;
+		}
+		public void setLibCategorie(String libCategorie) {
+			this.libCategorie = libCategorie;
+		}
+		
+		
+}
---
+++ projetJava/bin/projetJava/Conditionement.java
@@ -0,0 +1,21 @@
+
+public class Conditionement {
+
+	//attributs
+	private int idConditionement;
+	private String libConditionement;
+	
+	//getters and setters
+	public int getIdConditionement() {
+		return idConditionement;
+	}
+	public void setIdConditionement(int idConditionement) {
+		this.idConditionement = idConditionement;
+	}
+	public String getLibConditionement() {
+		return libConditionement;
+	}
+	public void setLibConditionement(String libConditionement) {
+		this.libConditionement = libConditionement;
+	}
+}
